08ba93303ee2d417b55b8a3dde8405c2af2e1270
elyssa97 <81210842+elyssa97@users.noreply.github.com>
2022-08-24 18:59:55 +0800

    Add files via upload

--- shipping_backbone_ns/__manifest__.py
+++ shipping_backbone_ns/__manifest__.py
@@ -2,13 +2,13 @@
 
 {
     "name": "Shipping Connector",
-    "version": "15.17.8.2022",
+    "version": "15.0",
     "category": "General",
     'summary': "Connect your Odoo erp with bunch of shipping provider like dhl, usps, dsv,fedex, tnt, ups, shipstation, gls, correos, easyship, amazone, shopify, woocommerce, ebay, etc",
     "description": """
         Backbone for Shipping Integration
     """,
-    "author": "NsInfosystem",
+    "author": "Wizeewig (Softlakes Sdn. Bhd)",
     "depends": ['delivery'],
     "data": [
         'security/ir.model.access.csv',
@@ -22,12 +22,9 @@
 
     'images': ['static/description/cover.jpeg'],
 
-    "website": "https://nsinfosystem.com/",
-    'support': 'contact@nsinfosystem.com/',
-    'maintainer': 'NsInfosystem',
-
-    "price": "9.99",
-    "currency": "EUR",
+    "website": "https://www.wiz.asia",
+    'support': 'help@wiz.asia',
+    'maintainer': 'Wizeewig (Softlakes Sdn. Bhd)',
     'license': 'OPL-1',
 }
 # 15.1
